47b198d792e0f8c280e6488417d26e20c721d505
Ademilton <ademilton.mariano@gmail.com>
2021-09-17 14:55:47 -0300

    inclusão do trabalho HTML

---
+++ HTML (Hotel)/js_mariano.js
@@ -0,0 +1,12 @@
+function Individual() {
+alert("Você acaba de ganhar 10% de desconto na sua reserva!");
+}
+function Casal() {
+    alert("Você acaba de ganhar 20% de desconto na sua reserva!");
+    }
+    function Tril() {
+        alert("Você acaba de ganhar 30% de desconto na sua reserva!");
+        }
+        function Quadra() {
+            alert("Você acaba de ganhar 40% de desconto na sua reserva!");
+            }
